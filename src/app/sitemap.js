70ce59aaabd1771cfb3986225bd05d9a21ed9035
Shahmar Kazimov <shahmar.kazimoff@gmail.com>
2026-05-27 18:55:34 +0400

    _

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,24 +1,58 @@
 import homes from "../components/data/homes";
 
 export default function sitemap() {
-  const defaultHomeUrls = homes.map((home) => ({
-    url: `https://area36.az/${home.slug}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 0.8,
-  }));
-
-  const azHomeUrls = homes.map((home) => ({
-    url: `https://area36.az/az/${home.slug}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 0.8,
-  }));
+  const homeUrls = homes.flatMap((home) => [
+    {
+      url: `https://area36.az/${home.slug}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: `https://area36.az/${home.slug}`,
+          az: `https://area36.az/az/${home.slug}`,
+        },
+      },
+    },
+    {
+      url: `https://area36.az/az/${home.slug}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: `https://area36.az/${home.slug}`,
+          az: `https://area36.az/az/${home.slug}`,
+        },
+      },
+    },
+  ]);
 
   return [
-    { url: "https://area36.az", lastModified: new Date(), priority: 1, changeFrequency: "daily" },
-    { url: "https://area36.az/az", lastModified: new Date(), priority: 1, changeFrequency: "daily" },
-    ...defaultHomeUrls,
-    ...azHomeUrls,
+    {
+      url: "https://area36.az",
+      lastModified: new Date(),
+      priority: 1,
+      changeFrequency: "daily",
+      alternates: {
+        languages: {
+          en: "https://area36.az",
+          az: "https://area36.az/az",
+        },
+      },
+    },
+    {
+      url: "https://area36.az/az",
+      lastModified: new Date(),
+      priority: 1,
+      changeFrequency: "daily",
+      alternates: {
+        languages: {
+          en: "https://area36.az",
+          az: "https://area36.az/az",
+        },
+      },
+    },
+    ...homeUrls,
   ];
 }
